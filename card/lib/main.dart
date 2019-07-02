309efce36b5a5151e118701ebf4672533c9300ca
Quang Son Le <quangle.bxl@gmail.com>
2019-07-02 13:50:18 +0200

    add assets and FontManifest.json

--- card/lib/main.dart
+++ card/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 void main() => runApp(App());
 
@@ -18,17 +19,23 @@ class App extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.center,
             // crossAxisAlignment: CrossAxisAlignment.center,
             children: <Widget>[
-              Spacer(flex: 1),
+              Spacer(flex: 3),
               Container(
                 padding: EdgeInsets.all(2.0),
                 decoration: BoxDecoration(
                   shape: BoxShape.circle,
                   color: Colors.white,
                 ),
-                child: CircleAvatar(
-                  backgroundImage: AssetImage('assets/images/IMG_7948.JPG'),
-                  //child: Text('ME!'),
-                  radius: 60,
+                child: RawMaterialButton(
+                  onPressed: () {
+                    _launchURL(
+                        'https://www.linkedin.com/in/quang-son-le-2b88988/');
+                  },
+                  child: CircleAvatar(
+                    backgroundImage: AssetImage('assets/images/IMG_7948.JPG'),
+                    //child: Text('ME!'),
+                    radius: 60,
+                  ),
                 ),
               ),
               SizedBox(height: 15.0),
@@ -48,67 +55,19 @@ class App extends StatelessWidget {
                       fontSize: 25,
                       color: Colors.white,
                       fontFamily: 'JuliusSansOne')),
-              SizedBox(height: 25.0),
-              Builder(builder: (context) {
-                double screenWidth = MediaQuery.of(context).size.width;
-                return Container(
-                    width: screenWidth > 320
-                        ? screenWidth * 0.7
-                        : screenWidth * 0.9,
-                    color: Colors.white,
-                    //color: Colors.white,
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: <Widget>[
-                          Icon(
-                            Icons.phone,
-                          ),
-                          SizedBox(width: 25.0),
-                          Text(
-                            '+32 477606291',
-                            style: TextStyle(
-                              fontFamily: 'UbuntuMono',
-                              fontSize: 18,
-                            ),
-                          ),
-                          //Spacer(),
-                        ],
-                      ),
-                    ));
-              }),
-              SizedBox(height: 20.0),
-              Builder(builder: (context) {
-                double screenWidth = MediaQuery.of(context).size.width;
-                print(screenWidth);
-                return Container(
-                  width:
-                      screenWidth > 320 ? screenWidth * 0.7 : screenWidth * 0.9,
-                  color: Colors.white,
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: <Widget>[
-                        Icon(
-                          Icons.email,
-                        ),
-                        SizedBox(width: 25.0),
-                        Text(
-                          'dev.quang.le@gmail.com',
-                          style: TextStyle(
-                            fontFamily: 'UbuntuMono',
-                            fontSize: 18,
-                          ),
-                        ),
-                        Spacer(),
-                      ],
-                    ),
-                  ),
-                );
-              }),
-              Spacer(flex: 2),
+              SizedBox(
+                  height: 25.0,
+                  width: 150,
+                  child: Divider(height: 10, color: Colors.white)),
+              _contactField(context, Icons.phone, 'tel: +32477606291',
+                  '+32 477 60 62 91'),
+              SizedBox(height: 10.0),
+              _contactField(
+                  context,
+                  Icons.email,
+                  'mailto:dev.quang.le@gmail.com?subject=contact%20via%20your%20app&body=Say%20something%20nice%20here:',
+                  'dev.quang.le@gmail.com'),
+              Spacer(flex: 4),
               Text(
                 "Made with Flutter",
                 style: TextStyle(color: Colors.blueGrey[700]),
@@ -119,5 +78,61 @@ class App extends StatelessWidget {
         ),
       )),
     );
+  }
+
+  Widget _contactField(BuildContext context, IconData icon, String contactUrl,
+      [String contactToDisplay]) {
+    return Builder(builder: (context) {
+      double screenWidth = MediaQuery.of(context).size.width;
+      return RawMaterialButton(
+        onPressed: () {
+          _launchURL(contactUrl);
+        },
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            shape: BoxShape.rectangle,
+            color: Colors.white,
+          ),
+          width: screenWidth > 320 ? screenWidth * 0.7 : screenWidth * 0.9,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                Icon(icon, color: Colors.blue[900]),
+                SizedBox(width: 25.0),
+                Text(
+                  contactToDisplay ?? contactUrl,
+                  style: TextStyle(
+                    fontFamily: 'UbuntuMono',
+                    fontSize: 18,
+                    color: Colors.blue[900],
+                  ),
+                ),
+                Spacer(),
+              ],
+            ),
+          ),
+        ),
+      );
+    });
+  }
+
+  void _launchURL(String url) async {
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
+  //The problem with card is that you have to use margins to define the width.
+  // I prefer to use Mediaquery to make it responsive
+  Widget _cardExample() {
+    return Card(
+        margin: EdgeInsets.all(50),
+        child: ListTile(
+            leading: Icon(Icons.phone), title: Text('contact to display')));
   }
 }
